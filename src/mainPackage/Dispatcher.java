0352f7e2f5e4000004e959775373d1241e07d00d
me <saveliev@dlit.dp.ua>
2017-08-11 13:01:35 +0300

    First commit (making a pattern)

--- src/mainPackage/Dispatcher.java
+++ src/mainPackage/Dispatcher.java
@@ -1,4 +1,26 @@
 package mainPackage;
 
+import java.util.Scanner;
+
+// First class with main void
+
 public class Dispatcher {
+
+    private static Scanner scanner;
+
+    public static void main(String[] args) {
+
+    }
+
+    private static void print(Object o) {
+        System.out.print(o);
+    }
+
+    private static void println(Object o) {
+        System.out.println(o);
+    }
+
+    private static void println() {
+        System.out.println();
+    }
 }
